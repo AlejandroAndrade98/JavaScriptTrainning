a07f9b0d88a1851a52b918a3ac18b4ff63203998
Alejandro Anibal Andrade <alejandro21112@hotmail.com>
2025-08-10 17:34:05 -0500

    saving the 4th example of functions

--- ACFunctions.js
+++ ACFunctions.js
@@ -97,3 +97,22 @@ function convertCtoF(number) {
 
 console.log(convertCtoF(-30)); // -22
 console.log(convertCtoF(-10)); // 14
+
+                                                                              // Example4 of function //
+
+let year = 2024; 
+
+function isLeapYear(year) {
+  if (year % 400 === 0) {
+    return `${year} is a leap year.`;
+  } else if (year % 100 === 0) {
+    return `${year} is not a leap year.`;
+  } else if (year % 4 === 0) {
+    return `${year} is a leap year.`;
+  } else {
+    return `${year} is not a leap year.`;
+  }
+}
+
+const result = isLeapYear(year);
+console.log(result);
